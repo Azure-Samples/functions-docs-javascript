e67cc1cc8f439df3203315fd9611882329837804
Dina Berry (MSFT) <diberry@microsoft.com>
2024-04-23 22:59:49 +0000

    v4

---
+++ functions-add-output-binding-storage-queue-cli-v4-programming-model/src/functions/httpTrigger1.js
@@ -0,0 +1,34 @@
+const { app, output } = require('@azure/functions');
+
+const sendToQueue = output.storageQueue({
+    queueName: 'outqueue',
+    connection: 'AzureWebJobsStorage',
+});
+
+app.http('HttpExample', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    extraOutputs: [sendToQueue],
+    handler: async (request, context) => {
+        
+        try {
+            context.log(`Http function processed request for url "${request.url}"`);
+
+            const name = request.query.get('name') || await request.text();
+            context.log(`Name: ${name}`);
+
+            if (name) {
+                const msg = `Name passed to the function ${name}`
+                context.extraOutputs.set(sendToQueue, [msg]);
+                return { body: msg };
+            } else {
+                context.log('Missing required data');
+                return { status: 404, body: 'Missing required data' };
+            }
+
+        } catch (error) {
+            context.log(`Error: ${error}`);
+            return { status: 500, body: 'Internal Server Error' };
+        }
+    }
+});
